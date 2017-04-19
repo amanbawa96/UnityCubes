420a4b16dd402c4a9b974efc16269b5bf052715a
amanbawa96 <amanb@uw.edu>
2017-04-19 16:20:17 -0700

    Adding all files

---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 move = new Vector3 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f );
+
+		rb.AddForce (move*speed);
+	}
+}
